43fab3af3fcc6958bc127ef82b2d17a777d15e0f
Fotis Aronis <aronisfotis@gmail.com>
2018-12-05 17:53:00 +0200

    first commit

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorChanger : MonoBehaviour {
+
+	public Color colorRed = Color.red;
+	public Color colorBlue = Color.blue;
+
+	public float duration = 3.0f; 
+
+	public Camera cam; 
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera> ();
+		cam.clearFlags = CameraClearFlags.SolidColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			float t = Mathf.PingPong (Time.time, duration);
+			cam.backgroundColor = Color.Lerp (colorRed, colorBlue, t);
+		}
+
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rotator : MonoBehaviour {
+
+	public Sprite check; 
+	public Button button; 
+
+
+	private bool done = false;
+
+	// Use this for initialization
+	void Start () {
+		button = GetComponent<Button> ();
+	}
+
+	public void OnMouseDown() {
+		transform.Rotate (0,180,0);
+		done = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//transform.Rotate (0, Time.deltaTime, 0);
+
+		if (transform.rotation.eulerAngles.y == 180) {
+			button.GetComponent<Image> ().sprite = check;
+		}
+
+		if (done) {
+			transform.Rotate (0, 0, 0);
+		}
+	}
+}
