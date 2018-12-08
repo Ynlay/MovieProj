54e7bbe62b56fdcacf836056d9243d410ccf4fcd
Fotis Aronis <aronisfotis@gmail.com>
2018-12-08 13:32:43 +0200

    update

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour {
+
+	public Text i; 
+
+	private float alpha; 
+
+	public void Start() { 
+		i.color = new Color (i.color.r, i.color.g, i.color.b, 0);
+	}
+
+	public void AdjustAlpha(float newAlpha) {
+		i.color = new Color(i.color.r, i.color.g, i.color.b, newAlpha);
+	}
+
+//	public IEnumerator FadeTextToFullAlpha() {
+//		i.color = new Color (i.color.r, i.color.g, i.color.b, 0);
+//		while (i.color.a < 1.0f) {
+//			i.color = new Color (i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+//			yield return null;
+//		}
+//	}
+//
+//	public IEnumerator FadeTextToZeroAlpha() {
+//		i.color = new Color (i.color.r, i.color.g, i.color.b, 1);
+//		while (i.color.a > 0.0f) {
+//			i.color = new Color (i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+//			yield return null;
+//		}
+//	}
+
+}
---
+++ Assets/Scripts/FingerPrintActivation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FingerPrintActivation : MonoBehaviour {
+
+	public Image img; 
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		img.color = new Color (img.color.r, img.color.g, img.color.b, 0.3f); 	
+	}
+
+	public void OnClick() {
+		StartCoroutine (AdjustAlpha ());
+	}
+
+	public IEnumerator AdjustAlpha() {
+		while (img.color.a < 1.0f) {
+			img.color = new Color (img.color.r, img.color.g, img.color.b, img.color.a + (Time.deltaTime / speed));
+			yield return null;
+		}
+	}
+
+}
